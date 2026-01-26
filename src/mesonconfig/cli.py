2a6e683f1c5d2d00cc44ca600d1ad1151788e0e3
NTSP3 <ntos32bit@gmail.com>
2026-01-26 09:43:11 +0530

    Added argument checking, removed old test code

--- src/mesonconfig/cli.py
+++ src/mesonconfig/cli.py
@@ -1,55 +1,53 @@
 #
-# Command-line TUI interface for Mesonconfig
+# Regular Command-line interface for Mesonconfig
 # 2026, Remeny
 #
 
 # ---[ Libraries ]--- #
 from .core import *
+from .tui import *
+import shutil
 
-from textual.app import App
-from textual.screen import ModalScreen
-from textual.widgets import Button, Label
-from textual.containers import Vertical, Center
-
-class OkDialog(ModalScreen):
-
-    CSS = """
-    #box {
-        width: 40;
-        height: 7;
-        padding: 1;
-        border: round white;
-        background: $surface;
-    }
-
-    Label {
-        content-align: center middle;
-        height: 3;
-    }
-
-    Button {
-        width: 10;
-        align-horizontal: center;
-    }
-    """
-
-    def compose(self):
-        yield Center(
-            Vertical(
-                Label("no this doesnt look like a dialog."),
-                Button("OK", id="ok"),
-                id="box"
-            )
-        )
-
-    def on_button_pressed(self, event: Button.Pressed):
-        self.dismiss()
-
-
-class DialogApp(App):
-    def on_mount(self):
-        self.push_screen(OkDialog())
+# ---[ Variables ]--- #
+text_screen_size_less_than_min: str = ("Your display is too small to run Mesonconfig!\n"
+                                      f"It must be at least {min_rows} lines by {min_cols} columns."
+                                      )
 
 # ---[ Entry point ]--- #
 def main():
-    DialogApp().run()
+    # Argument checking.
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--version", action="store_true")
+    args = parser.parse_args()
+
+    if args.version:
+        print("")
+        print("Mesonconfig")
+        print(get_version())
+        print("Repository: github.com/stellcel-remeny/mesonconfig")
+        print("")
+        return
+
+    try:
+        # Check if the terminal size is greater than or equal to minimum
+        screen_size = shutil.get_terminal_size(fallback=(0, 0))
+        if screen_size.columns < min_cols or screen_size.lines < min_rows:
+            raise TerminalTooSmall
+        
+        MCfgApp().run()
+    
+    except TerminalTooSmall:
+        # Screen size is too small.
+        screen_size = shutil.get_terminal_size(fallback=(0, 0))
+        print(text_screen_size_less_than_min)
+        print(f"Current size: {screen_size.columns}x{screen_size.lines}")
+    
+    except Exception as e:
+        import traceback
+        print("Oops; An UNexpected error! :(")
+        traceback.print_exc()
+        print(" ) Figure out the error.")
+
+if __name__ == "__main__":
+    main()
