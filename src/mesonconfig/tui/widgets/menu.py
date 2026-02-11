58b631d10465b59affcc58daf52bcebe75643d18
NTSP3 <ntos32bit@gmail.com>
2026-02-11 23:03:22 +0530

    Added button select indicators for dbg, fixed issue with list index

--- src/mesonconfig/tui/widgets/menu.py
+++ src/mesonconfig/tui/widgets/menu.py
@@ -4,6 +4,8 @@
 #
 
 # TODO: Make MenuDisplay and other windows 3D (shadow)
+# TODO: Add a quit screen on Exit selected
+# TODO: Add real functionality on button pressed
 
 # ---[ Libraries ]--- #
 # textual tui libs
@@ -61,13 +63,45 @@ class MenuDisplay(Static):
             yield self.control_bar
 
     def on_list_view_selected(self, event: ListView.Selected) -> None:
-        index = int(event.item.id.split("_")[1])
+        index = self.list_view.index
         value = self.items[index]
         self.app.dbg(f"Selected: {value}")
 
     def on_mount(self):
         self.border_title = f"[bold]{self.title}[/bold]"
         self.update_items(self.items)
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        button_id = event.button.id
+
+        if button_id == "btn_exit":
+            # TODO: Add a Quit screen
+            self.app.exit()
+
+        elif button_id == "btn_select":
+            self.handle_select()
+
+        elif button_id == "btn_help":
+            self.handle_help()
+
+        elif button_id == "btn_save":
+            self.handle_save()
+
+        elif button_id == "btn_load":
+            self.handle_load()
+
+    # TODO: Add actual functionality
+    def handle_select(self):
+        self.app.dbg("Select pressed")
+
+    def handle_help(self):
+        self.app.dbg("Help pressed")
+
+    def handle_save(self):
+        self.app.dbg("Save pressed")
+
+    def handle_load(self):
+        self.app.dbg("Load pressed")
 
     def update_items(self, items: list[str]) -> None:
         """Replace the items in the ListView."""
